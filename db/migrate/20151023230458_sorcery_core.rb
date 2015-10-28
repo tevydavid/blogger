b7409a56974afa7b79c1578b1f9d7c4f495e53c3
Tevy Jacobs-Gomes <tevydavid@Tevys-MacBook-Pro.local>
2015-10-27 18:12:58 -0700

    First

---
+++ db/migrate/20151023230458_sorcery_core.rb
@@ -0,0 +1,29 @@
+=begin
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :email,            :null => false
+      t.string :crypted_password
+      t.string :salt
+
+      t.timestamps
+    end
+
+    add_index :authors, :email, unique: true
+  end
+end
+=end
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :username,         :null => false
+      t.string :email,            :null => false
+      t.string :crypted_password, :null => false
+      t.string :salt,             :null => false
+
+      t.timestamps
+    end
+
+    add_index :authors, :email, unique: true
+  end
+end
